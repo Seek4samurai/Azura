55db25fa09b795b9b80502cb26223e4e84b7531a
Gourav Singh Rawat <grvrawat999@gmail.com>
2022-02-18 22:20:05 +0530

    ux: implemented chrome:tts

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,15 @@
 const paragraph = document.getElementById("textp");
 const input = document.getElementById("imgAddress");
 const submitButton = document.getElementById("submit");
+const confidenceScore = document.getElementById("confidenceScore");
 
 async function identify() {
   const url = input.value;
-  const res = await axios.post("http://localhost:5000/describe", {
+  const res = await axios.post("https://azura-backend.herokuapp.com/describe", {
     imageUrl: url,
   });
   paragraph.innerText = res.data;
+  await chrome.tts.speak(res.data, { 'lang': 'en-US', 'rate': 1.0 });
 }
 
 submitButton.addEventListener("click", identify)
